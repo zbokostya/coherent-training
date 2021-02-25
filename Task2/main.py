9a02623878ff9dd348777da98e8299b2eefb89aa
zbokostya <kostya.zboichik@gmail.com>
2021-02-26 00:30:30 +0300

    Task2 v1.1

--- Task2/main.py
+++ Task2/main.py
@@ -3,53 +3,54 @@ import argparse
 import time
 import logging
 
-DATA_DIR = 'data/ml-latest-small/'
+DATA_DIR = 'data/ml-25m/'
 
 
-def get_films_id_by_genres_year(genres, year_from, year_to):
+def get_films_id_by_genres_year(genres, year_from, year_to, regexp):
     with open(DATA_DIR + 'movies.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        films = []
-        for row in spamreader:
+        films_csv = csv.reader(csvfile, delimiter=',', quotechar='"')
+        films = dict()
+        for row in films_csv:
             for genre in genres:
-                year_index = row[1].rfind('(')
-                try:
-                    if genre in row[2] and year_from <= int(row[1][year_index + 1:year_index + 5]) <= year_to:
-                        films.append(row[0])
-                        break
-                except ValueError:
-                    logging.error(row)
-                    continue
+                parse_year = year_parse(row[1])
+
+                if genre in row[2] and year_from <= parse_year[0] <= year_to and regexp in row[1]:
+                    films[row[0]] = [row[2], row[1][0:parse_year[1]], parse_year[0], [0, 0]]
+                    break
         return films
 
+def year_parse(name):
+    year_index = name.rfind('(')
+    try:
+        year = int(name[year_index + 1:year_index + 5])
+    except ValueError:
+        year = 0
+        year_index = len(name)
+    return (year, year_index)
 
 def count_ratings(films, count_n):
-    ratings = dict()
-    for film in films:
-        ratings[film] = [0.0, 0]
-    with open(DATA_DIR + 'ratings.csv', newline='') as csvfile2:
-        spamreader = csv.reader(csvfile2, delimiter=',', quotechar='"')
-        for row in spamreader:
-            if row[1] in ratings:
+    with open(DATA_DIR + 'ratings.csv', newline='') as csvfile:
+        films_rating_csv = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in films_rating_csv:
+            if row[1] in films:
                 try:
-                    ratings[row[1]] = [ratings[row[1]][0] + float(row[2]), ratings[row[1]][1] + 1]
+                    films[row[1]][3] = [films[row[1]][3][0] + float(row[2]), films[row[1]][3][1] + 1]
                 except ValueError as e:
                     print(e)
                     continue
-        for el in ratings:
-            if ratings[el][1] != 0:
-                ratings[el] = ratings[el][0] / ratings[el][1]
+        for el in films:
+            if films[el][3][1] != 0:
+                films[el][3] = films[el][3][0] / films[el][3][1]
             else:
-                ratings[el] = 0
-        return dict(sorted(ratings.items(), key=lambda item: item[1], reverse=True)[:count_n])
+                films[el][3] = 0
+        return sorted(films.items(), key=lambda item: item[1][3], reverse=True)[:count_n]
 
 
 def arg_parse():
-    # tofix
     parser = argparse.ArgumentParser(description='Abc')
     parser.add_argument('-N', default=10, type=int, help='')
     parser.add_argument('-genres', default='', type=str, help='')
-    parser.add_argument('-year_from', default=0, type=int, help='')
+    parser.add_argument('-year_from', default=-1, type=int, help='')
     parser.add_argument('-year_to', default=9999, type=int, help='')
     parser.add_argument('-regexp', default='', type=str, help='')
     return parser.parse_args()
@@ -61,22 +62,15 @@ def parse_films_genres(films_arg):
 
 def create_csv_like_format(top_ratings):
     print('genre, title, year, rating')
-
-    with open(DATA_DIR + 'movies.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        films = []
-        for row in spamreader:
-            if row[0] in top_ratings:
-                print((row[2], row[1], top_ratings[row[0]]))
-                films.append((row[2], row[1], top_ratings[row[0]]))
-        films = sorted(films, key=lambda item: item[2], reverse=True)
-        for film in films:
-            print(film)
+    for el in top_ratings:
+        # print(top_ratings[el])
+        # print(top_ratings[el][0] + ',' + top_ratings[el][1] + ',' + top_ratings[el][2] + ',' + top_ratings[el][3])
+        print(el[1][0] + ',' + el[1][1] + ',' + str(el[1][2]) + ',' + str(el[1][3]))
 
 
 def main(args):
     genres = parse_films_genres(args.genres)
-    films = get_films_id_by_genres_year(genres, args.year_from, args.year_to)
+    films = get_films_id_by_genres_year(genres, args.year_from, args.year_to, args.regexp)
     ratings = count_ratings(films, count_n=args.N)
     create_csv_like_format(ratings)
 
