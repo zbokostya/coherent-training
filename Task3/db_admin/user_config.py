7476ff2babaaab02daa7f45ea306e1ff16d11405
zbokostya <kostya.zboichik@gmail.com>
2021-04-25 18:26:53 +0300

    change user config

--- Task3/db_admin/user_config.py
+++ Task3/db_admin/user_config.py
@@ -3,7 +3,7 @@ from os import path
 file_folder_path = path.abspath(path.dirname(path.dirname(__file__)))
 
 config = {
-    'user': 'root',
+    'user': 'zbokostya',
     'password': 'zFh3gm0BLnwb',
     'port': '3306',
     'host': '127.0.0.1'
